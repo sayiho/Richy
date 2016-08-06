e6f4993265d799fd85b133c6f1b49fd689aecb2e
michpcx <MichPCX@users.noreply.github.com>
2016-08-06 17:10:54 +0100

    Updated Heinrichy to _0.35_Alpha
    New _0.35_Alpha version of Heinrichy!

--- README.md
+++ README.md
@@ -4,62 +4,76 @@ Personal assistant made especially for GNU/Linux because we deserve our own vers
 
 ![alt Heinrichy](https://i.imgur.com/63pl8Ob.png)
 
-Did you ever wanted to have your own assistant, but the only ones available are for Android/iOS/Windows plus you are 
-worried about your personal informations? 
+Did you ever wanted to have your own assistant, but the only ones available are for Android/iOS/Windows plus you are
+worried about privacy?
 
-Well, now you can have '**Heinrichy**' which will help you to live in the terminal, because what's the point of searching
-the web which can take some time when you can simply just ask Heinrichy?
+Well, now you can have '**Heinrichy**' which will help you to live in the terminal, because what's the point of searching the web which can take a while when you can simply just ask Heinrichy?
 
 Heinrichy is python based assistant which can help you with;
 - maintaining your schedule
-- aswer your questions
-- calculate medium-level calculation
-And many more modules coming soon!
+- aswering your questions
+- calculating medium-level calculation
+- giving you informations about movies on the local drive
 
-## Current version
+## Curent version
+
 The most recent version is _0.27_Alpha which brings:
+- Changed config format from py to conf (loading using configparser) for flexibility
+- Added ability to turn on/off schedule module, multimedia module and modules that will come in the future
+- New module called multimedia which allows to give you more info about movies you have on the local hard drive
+- first_timer.py script which checks if you have required packages installed to run Heinrichy
 
-- Python 2 and python 3 compatibilty so you can use Heinrichy on most of the linux distros now!
-- Schedule is now been moved to schedule.json which gives more room to work it with
-- Few removals and bug fixes which speeds up Heinrichy
 
 ## Installation
-After downloading Heinrichy, run `pip install beautifulsoup` to install Beautiful Soup module which is required. After that all that is left for you to do is to fill in the config file where the values say 'default' (you can also change some other
-values which is optional) and then just run 'heinrichy_main.py'.
+After downloading Heinrichy, fill in the config file where the values say 'default' (you can also change some other
+values which is optional), run first_timer.py script which will analyse your environment and if it won't
+give you any errors, it means you can run Heinrichy by running 'heinrichy_main.py'.
 
 ## Basic questions
 ```
+'how are you?'
 'schedule'
-'whats the day today?'
+'is the martian any good?'
 'whats 100*42?'
-'who was 40th president of united states of america?'
-'when did binladen died?'
+'rhymes to happy'
 ```
 
 ## To-do
-- [ ] *Search for movies on the hard drive and give info about them*
-- [ ] *News from chosen websites*
-- [ ] *Show weather*
-- [ ] *Give movie/book/music description* 
-- [ ] *Currency conversion*
-- [ ] *Random fact/jokes/quotes*
-- [ ] *Dictionary*
+- [x] *Search for movies on the hard drive and give info about them*
 - [ ] *Wikipedia*
+- [ ] *Dictionary*
+- [ ] *Show weather*
+- [ ] *Improve calculator*
+- [ ] *Currency conversion*
 - [ ] *Lyrics for chosen song*
-- [ ] *Url expander/shortener* 
+- [ ] *Url expander/shortener*
+- [ ] *Random fact/jokes/quotes*
+- [ ] *News from chosen websites*
+- [ ] *Allow to install Heinrichy*
+- [ ] *Download subtitles for movies*
+- [ ] *Move commands into the database*
+- [ ] *Give movie/book/music description*
+
+## Modules 
+
+So far, Heinrichy has those modules;
+- Schedule (external) - allows to modify your schedule which will then be displayed in Heinrichy
+- Multimedia (internal) - allows to get more information about movies you have on the local drive
+
+And many more modules and features coming soon!
 
 ## Why Heinrichy?
-Name 'Heinrichy' is a modification after the name of famous physicist [Heinrich Friedrich Weber](https://en.wikipedia.org/wiki/Heinrich_F._Weber) which was Albert Einstein's initial doctoral 
+Name 'Heinrichy' is a modification after the name of famous physicist [Heinrich Friedrich Weber](https://en.wikipedia.org/wiki/Heinrich_F._Weber) which was Albert Einstein's initial doctoral
 advisor. As Heinrichy was helping Albert Einstein, Heinrichy will help you with day-to-day questions and tasks.
 It was made especially for GNU/Linux as members of Linux Master Race also need an assistant, however with few
 code modifications, the code should work on Microsoft Windows or MacOS. I've decided to create Heinrichy after
 I came across an article where the author was trying to live in the terminal and only use the terminal without any GUI for
-30 days. This is nearly impossible without any preparation which is why I created Heinrichy, I wanted to do 
-everything in one place in the command line without the need for any user interface.
+30 days. This is nearly impossible without any preparation which is why I created Heinrichy, I wanted to do
+nearly everything in the command line without the need for any user interface.
 
 ## How can I help?
-If you have any ideas on what I could add to Heinrichy, how I could change it or if you found some interesting
-script that I could use as module in Heinrichy, just email me; michpcx@protonmail.ch. Also, if you found some
-bugs in the code or you have more efficient way of performing a task, just create new issue in the [Issue tracker](https://github.com/MichPCX/Heinrichy/issues).
+If you have any ideas on how Heinrichy could be improved or if you found some interesting
+script that could be used as module to Heinrichy, just email me; michpcx@protonmail.ch. Also, if you found some
+bugs in the code or you have more efficient way of performing a task, just create new issue in the [issue tracker](https://github.com/MichPCX/Heinrichy/issues).
 
 *By michpcx, michpcx@protonmail.ch*
--- heinrichy_main.py
+++ heinrichy_main.py
@@ -5,74 +5,28 @@
 #              | |_| |/ _ | | '_ \| '__| |/ __| '_ \| | | |
 #              |  _  |  __| | | | | |  | | (__| | | | |_| |
 #              |_| |_|\___|_|_| |_|_|  |_|\___|_| |_|\__, |
-#                                                    |___/           _0.27_Alpha
+#                                                    |___/           _0.35_Alpha
 #
 #   Heinrichy - personal assistant made especially for GNU/Linux because we
 #                   deserve our own version of siri too!
-#								      by michpcx
+#								         by michpcx
 
+
+
+# Modules to import first and setting variables
 from __future__ import print_function
-
-# Modules to import first before anything
+print("Loading main modules...")
 import os
 import sys
 import json
-current_path = os.path.dirname(os.path.abspath(__file__))
-
-
-# Checking for config file
-config_file = current_path + "/config.py"
-is_config_file = os.path.isfile(config_file)
-
-if not is_config_file:
-    print("Heinrichy - personal assistant made especially for GNU/Linux because we deserve our own version of siri too!")
-    print("Heinrichy wasn't able to detect config file which is required to run, please redownload\n Heinrichy with this file...")
-    print("Exiting...")
-    sys.exit()
-else:
-    import config
-
-    # Loading user info
-    name = os.environ.get('Name', config.name)
-    date_of_birth = os.environ.get('Date of birth', config.date_of_birth)
-    sex = os.environ.get('Sex', config.sex)
-
-    # Loading settings
-    show_version = os.environ.get('Show version', config.show_version)
-    clear_commands = os.environ.get('Clear commands', config.clear_commands)
-    additional_search = os.environ.get('Additional search', config.additional_search)
-    letter_color = os.environ.get('Color of the letters', config.letter_color)
-
-    # Loading schedule settings
-    show_schedule = os.environ.get('Show schedule', config.show_schedule)
-    schedule_date_format = os.environ.get('Schedule date format', config.schedule_date_format)
-    print("Config file loaded...")
-
-
-# Checking for schedule file & schedule module
-print("Loading schedule module...")
-schedule_file = current_path + "/schedule.json"
-schedule_module = current_path + "/modules/schedule.py"
-is_schedule_file = os.path.isfile(schedule_file)
-is_schedule_module = os.path.isfile(schedule_module)
-
-if not is_schedule_file or not is_schedule_module:
-    print("Heinrichy wasn't able to detect schedule module or schedule file which is required to run, please redownload\n Heinrichy with this file...")
-    print("Exiting...")
-    sys.exit()
-else:
-    with open(schedule_file, "r+") as schedule_file_open:
-        schedule = json.load(schedule_file_open)
-
-# Loading additional modules, setting important variables and changing the size of terminal
-print("Loading main modules...")
 import time
 import random
 import httplib2
-from timeit import timeit
 from time import sleep
-import xml.etree.ElementTree as ET
+from timeit import timeit
 from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
+current_path = os.path.dirname(os.path.abspath(__file__))
 
 try:
     # Python2
@@ -92,31 +46,82 @@ schedule_list_for_today = [];
 print("Changing the size of the terminal...")
 sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=32, cols=115))
 
-print("Setting up the format of the dates...")
-if schedule_date_format == "DD/MM/YYYY":
-    schedule_date_format_type = 1
-    todays_date = str(time.strftime("%d/%m/%Y"))
-elif schedule_date_format == "MM/DD/YYYY":
-    schedule_date_format_type = 2
-    todays_date = str(time.strftime("%m/%d/%Y"))
-elif schedule_date_format == "YYYY/MM/DD":
-    schedule_date_format_type = 3
-    todays_date = str(time.strftime("%Y/%m/%d"))
-elif schedule_date_format == "YYYY/DD/MM":
-    schedule_date_format_type = 4
-    todays_date = str(time.strftime("%Y/%d/%m"))
-else:
-    print("Invalid date format, please change 'schedule_date_format' in config file...")
+
+
+# Checking for config file
+config_file = current_path + "/config.conf"
+is_config_file = os.path.isfile(config_file)
+
+if not is_config_file:
+    print("Heinrichy - personal assistant made especially for GNU/Linux because we deserve our own version of siri too!")
+    print("Heinrichy wasn't able to detect config file which is required to run, please redownload\n Heinrichy with this file...")
     print("Exiting...")
     sys.exit()
-todays_date = str(time.strftime("%d/%m/%Y"))
+else:
+    import configparser
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    # Loading user info
+    name = config.get('user_info', 'name')
+    date_of_birth = config.get('user_info', 'date_of_birth')
+
+    # Loading main settings
+    show_version = config.get('main_settings', 'show_version')
+    clear_commands = config.get('main_settings', 'clear_commands')
+    additional_search = config.get('main_settings', 'additional_search')
+    letter_color = config.get('main_settings', 'letter_color')
+
+    # Loading schedule module settings
+    schedule_module = config.get('schedule_module', 'schedule_module')
+    schedule_date_format = config.get('schedule_module', 'schedule_date_format')
+
+    # Loading multimedia module settings
+    multimedia_module = config.get('multimedia_module', 'multimedia_module')
+    movie_directory = config.get('multimedia_module', 'movie_directory')
+
+    print("Config file loaded...")
+
+
+
+# Loading schedule module & schedule file
+if schedule_module == "On":
+    print("Loading schedule module...")
+    schedule_file = current_path + "/schedule.json"
+    schedule_module_file = current_path + "/modules/schedule.py"
+    is_schedule_file = os.path.isfile(schedule_file)
+    is_schedule_module = os.path.isfile(schedule_module_file)
+
+    if not is_schedule_file or not is_schedule_module:
+        print("Heinrichy wasn't able to detect schedule module or schedule file which is required to run schedule module.\n Heinrichy will start without this module...")
+        schedule_module = "Off"
+        sleep(5)
+    else:
+        with open(schedule_file, "r+") as schedule_file_open:
+            schedule = json.load(schedule_file_open)
+
+
+
+# Loading multimedia module
+if multimedia_module == "On":
+    print("Loading multimedia module...")
+    multimedia_module_file = current_path + "/modules/multimedia.py"
+    is_multimedia_module = os.path.isfile(multimedia_module_file)
+
+    if not is_multimedia_module:
+        print("Heinrichy wasn't able to detect multimedia module which is required to run it.\n Heinrichy will start without this module...")
+        multimedia_module = "Off"
+        sleep(5)
+    elif is_multimedia_module:
+        import modules.multimedia
+
+
 
 # Classes & functions
 print("Loading classes and functions...")
 
-
-# Adds list with the colours so the colors can be displayed in the terminal
 class bcolors:
+    """ Load colors for the terminal """
     PINK = '\033[95m'
     BLUE = '\033[94m'
     YELLOW = '\033[93m'
@@ -127,9 +132,8 @@ class bcolors:
     UNDERLINE = '\033[4m'
     GREY = '\033[90m'
 
-
-# Function which lists the schedule.
 def list_schedule():
+    """ List the schedule """
     total_task_number_today = 0
     print("Your today's schedule;")
     for task in schedule["schedule_list"].keys():
@@ -142,6 +146,7 @@ def list_schedule():
 
 # Prints the main
 def print_main_screen():
+    """ Print main 'Heinrichy' text """
 
     # Main 'Heinrichy' text
     print("___________________________________________________________________________________________________________________")
@@ -155,23 +160,22 @@ def print_main_screen():
     print("|         " + color_changer + "888     888  888    .o  888   888   888   888      888  888   .o8  888   888     `888'" + bcolors.WHITE + "                  |")
     print("|        " + color_changer + "o888o   o888o `Y8bod8P' o888o o888o o888o d888b    o888o `Y8bod8P' o888o o888o     .8'" + bcolors.WHITE + "                   |")
     print("|                                                                                       " + color_changer + ".o..P'" + bcolors.WHITE + "                    |")
-    if show_version == True:
-        print("|                                                                                       " + color_changer + "`Y8P'" + bcolors.WHITE + "        _0.27_Alpha  |")
-    elif show_version == False:
+    if show_version == "True":
+        print("|                                                                                       " + color_changer + "`Y8P'" + bcolors.WHITE + "        _0.35_Alpha  |")
+    elif show_version == "False":
         print("|                                                                                       " + color_changer + "`Y8P'" + bcolors.WHITE + "                     |")
     print("|_________________________________________________________________________________________________________________|")
 
 def print_schedule():
-
-    # Printing schedule
+    """ Check if schedule module is on then print it """
     print("\n")
-    if show_schedule == True:
+    if schedule_module == "On":
         list_schedule()
-    elif show_schedule == False:
+    elif schedule_module == "Off":
         print("\n")
 
 def check_birthday():
-    # Checking if today is users birthday
+    """ Check if its users' birthday """
     if schedule_date_format_type == 1:
         if date_of_birth[:5] == str(time.strftime("%d/%m")):
             print(bcolors.YELLOW + "Happy Birthday, " + name + "!\n" + bcolors.WHITE)
@@ -185,7 +189,8 @@ def check_birthday():
         if date_of_birth[5:10] == str(time.strftime("%d/%m")):
             print(bcolors.YELLOW + "Happy Birthday, " + name + "!\n" + bcolors.WHITE)
 
-def response(query):                                                            # Function gets the xml file create from engine and gets plain text from it.
+def response(query):
+    """ Gets answer for users' query from wolframalpha servers """
     query = query.lower()
     query_original = query
     query = urlencode({'input':query})
@@ -205,11 +210,11 @@ def response(query):                                                            
     elif error:
         me_no_english1 = "Sorry, I didn't understood that."
         me_no_english2 = "I didn't get that."
-        me_no_english3 = "Ohh, english language is tough, I can't understand what you said."
+        me_no_english3 = "I can't understand what you said."
         me_no_english4 = "It seems like I can't answer this question."
         me_no_english = [me_no_english1, me_no_english2, me_no_english3, me_no_english4]
 
-        if additional_search == True:
+        if additional_search == "True":
             query_original = query_original.replace(" ", "_")
             r = urlopen("https://www.evi.com/q/" + query_original).read()
             whole_content = BeautifulSoup(r, "lxml")
@@ -222,7 +227,7 @@ def response(query):                                                            
             else:
                 return answer
 
-        elif additional_search == False:
+        elif additional_search == "False":
             return random.choice(me_no_english)
 
         elif additional_search == "Ask":
@@ -247,12 +252,8 @@ def response(query):                                                            
                 return "Wrong command."
 
 
-# -----------------------------------  Main  -----------------------------------
 
-sleep(1)
-clear()
-
-# Checking the colour of the letters
+# Setting up the colour of the letters
 if letter_color == "BLUE":
     color_changer = bcolors.BLUE
 elif letter_color == "GREY":
@@ -268,8 +269,36 @@ elif letter_color == "RED":
 elif letter_color == "WHITE":
     color_changer = bcolors.WHITE
 
-# Showing main text
-if clear_commands == True:
+
+
+# Setting up format of the dates
+print("Setting up the format of the dates...")
+if schedule_date_format == "DD/MM/YYYY":
+    schedule_date_format_type = 1
+    todays_date = str(time.strftime("%d/%m/%Y"))
+elif schedule_date_format == "MM/DD/YYYY":
+    schedule_date_format_type = 2
+    todays_date = str(time.strftime("%m/%d/%Y"))
+elif schedule_date_format == "YYYY/MM/DD":
+    schedule_date_format_type = 3
+    todays_date = str(time.strftime("%Y/%m/%d"))
+elif schedule_date_format == "YYYY/DD/MM":
+    schedule_date_format_type = 4
+    todays_date = str(time.strftime("%Y/%d/%m"))
+else:
+    print("Invalid date format, please change 'schedule_date_format' in config file  to suitable format.")
+    print("Exiting...")
+    sys.exit()
+todays_date = str(time.strftime("%d/%m/%Y"))
+
+
+
+# -----------------------------------  Main  -----------------------------------
+
+sleep(1)
+clear()
+
+if clear_commands == "True":
     while True:
 
         # Printing main 'Heinrichy' text
@@ -286,12 +315,34 @@ if clear_commands == True:
         print("How can I help you today, " + name + "?")
         user_input = input(">")
         if user_input == "schedule":
-            exec(compile(open(schedule_module).read(), schedule_module, 'exec'))
+            if schedule_module == "On":
+                exec(compile(open(schedule_module_file).read(), schedule_module_file, 'exec'))
+            elif schedule_module == "Off":
+                print("Schedule module has been turned off. Please change the config and restart Heinrichy.")
+                pause = input()
+        elif any(command in user_input for command in ("movies index", "movie index", "movies list", "movie list")) and multimedia_module == "Off":
+            print("multimedia module has been turned off. Please change the config and restart Heinrichy.")
+            pause = input()
+        elif any(command in user_input for command in ("movies index", "movie index", "movies list", "movie list")) and multimedia_module == "On":
+            if user_input == "movies index" or user_input == "movie index":
+                modules.multimedia.index_all()
+                pause = input()
+            elif user_input[:11] == "movies list" or user_input[:10] == "movie list":
+                if user_input.find("--") == -1:
+                    args = "none"
+                    modules.multimedia.list_all(args)
+                else:
+                    args = "--" + user_input.split('--', 1)[1]
+                    modules.multimedia.list_all(args)
+                pause = input()
+        elif user_input == "movies help" or user_input == "movie help":
+            modules.multimedia.movies_help()
+            pause = input()
         else:
             print(response(user_input))
             pause = input()
 
-elif clear_commands == False:
+elif clear_commands == "False":
 
     # Printing main 'Heinrichy' text
     print_main_screen()
@@ -304,13 +355,40 @@ elif clear_commands == False:
 
     # Asking for user input
     while True:
+
         print("How can I help you today, " + name + "?")
         user_input = input(">")
         if user_input == "schedule":
-            exec(compile(open(schedule_module).read(), schedule_module, 'exec'))
+            if schedule_module == "On":
+                exec(compile(open(schedule_module_file).read(), schedule_module_file, 'exec'))
+            elif schedule_module == "Off":
+                print("Schedule module has been turned off. Please change the config and restart Heinrichy.")
+                pause = input()
+        elif any(command in user_input for command in ("movies index", "movie index", "movies list", "movie list")) and multimedia_module == "Off":
+            print("multimedia module has been turned off. Please change the config and restart Heinrichy.")
+            pause = input()
+        elif any(command in user_input for command in ("movies index", "movie index", "movies list", "movie list")) and multimedia_module == "On":
+            if user_input == "movies index" or user_input == "movie index":
+                modules.multimedia.index_all()
+                pause = input()
+            elif user_input[:11] == "movies list" or user_input[:10] == "movie list":
+                if user_input.find("--") == 12:
+                    args = "--" + user_input.split('--', 1)[1]
+                    modules.multimedia.list_all(args)
+                elif user_input.find("--") == -1:
+                    args = "none"
+                    modules.multimedia.list_all(args)
+                else:
+                    print("Seems like there is an error with your command.")
+                pause = input()
+        elif user_input == "movies help":
+            modules.multimedia.movies_help()
+            pause = input()
         else:
             print(response(user_input))
             pause = input()
 
 else:
-    print("Variable clear_commands has invalid value, please change to True or False.")
+    print("Variable clear_commands has invalid value, please change it in config file to either True or False.")
+    print("Exiting...")
+    sys.exit()
